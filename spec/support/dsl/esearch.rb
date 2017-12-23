e67af5a6a6389c21ddb6a12f7f07b301d3a3f52c
eugen0329 <eugen0329@gmail.com>
2017-12-23 12:34:12 +0300

    fix esearch_config setting in ci

--- spec/support/dsl/esearch.rb
+++ spec/support/dsl/esearch.rb
@@ -3,8 +3,11 @@ module Support
     module ESearch
 
       def esearch_settings(options)
-        elems = options.map { |name, val| "'#{name}': '#{val}'" }
-        dict = "{ #{elems.join(',')} }"
+        pairs = options.map do |name, val|
+          val = "'#{val}'" unless val.is_a? Numeric
+          "'#{name}': #{val}"
+        end
+        dict = "{ #{pairs.join(',')} }"
 
         vim.normal(":if !exists('g:esearch') | let g:esearch = #{dict} | else | call extend(g:esearch, #{dict}) | endif<Enter><Enter>")
       end
